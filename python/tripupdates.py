a9d2d1be65a4e60c4af127248c0c4dc8f2681228
FlamingNineteen <flamingnineteen@gmail.com>
2026-08-17 13:46:39 -0700

    Implement trip updates for Availtec APIs

--- python/tripupdates.py
+++ python/tripupdates.py
@@ -1,13 +1,63 @@
 import gtfs_realtime_pb2 as gtfs_rt
+from utils import *
 
 import json
 import requests
 import time
 
-gtfsrt = gtfs_rt.FeedMessage()
+def GetTripUpdates():
+    gtfsrt = gtfs_rt.FeedMessage()
+        
+    header = gtfs_rt.FeedHeader()
+    header.gtfs_realtime_version = '2.0'
+    header.incrementality = gtfs_rt.FeedHeader.Incrementality.FULL_DATASET
+    header.timestamp = int(time.time())
+    gtfsrt.header.CopyFrom(header)
 
-with open('tripupdates.pb', 'rb') as file:
-    gtfsrt.ParseFromString(file.read())
+    uniqueid = 0
 
-with open('tripupdates copy.txt', 'w') as file:
-    file.write(str(gtfsrt))
+    # Availtec API
+    for url in (
+        'realtimevotran.availtec.com',
+        'realtimesuntran.availtec.com',
+        'www.ccbusinfo.com',
+    ):
+        print(url)
+        
+        get = requests.get(f'https://{url}/InfoPoint/rest/Routes/GetVisibleRoutes').text
+        son = json.loads(get)
+
+        for route in son:
+            for busdict in route.get('Vehicles'):
+                entity    = gtfs_rt.FeedEntity()
+                entity.id = str(uniqueid)
+                uniqueid += 1
+
+                update = gtfs_rt.TripUpdate()
+
+                trip         = gtfs_rt.TripDescriptor()
+                trip.trip_id = str(busdict.get('TripId'))
+                update.trip.CopyFrom(trip)
+
+                desc       = gtfs_rt.VehicleDescriptor()
+                desc.id    = str(busdict.get('VehicleId'))
+                desc.label = busdict.get('Name')
+                update.vehicle.CopyFrom(desc)
+
+                update.timestamp = formatdate(busdict.get('LastUpdated'))
+                update.delay     = busdict.get('Deviation')
+
+                entity.trip_update.CopyFrom(update)
+
+                gtfsrt.entity.add().CopyFrom(entity)
+
+    return gtfsrt
+
+if __name__ == '__main__':
+    result = GetTripUpdates()
+
+    with open('tripupdates.pb', 'wb') as file:
+        file.write(result.SerializeToString())
+
+    with open('tripupdates.txt', 'w') as file:
+        file.write(str(result))
